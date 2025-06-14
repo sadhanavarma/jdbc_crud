7284d55c226f5d3bcc2e798567d1cb85a0ccd0e6
sadhana1357varma@gmail.com <sadhana1357varma@gmail.com>
2025-06-14 22:41:46 +0530

    Initial commit with CreateDT.java

---
+++ src/jdbc_crud/CreateDT.java
@@ -0,0 +1,39 @@
+package jdbc_crud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class CreateDT {
+
+	public static void main(String[] args) {
+		
+		String url = "jdbc:mysql://localhost:3308/";
+		String user = "root";
+		String password = "root";
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			Connection con = DriverManager.getConnection(url, user, password);
+			Statement stmt = con.createStatement();
+			
+			String createDB = "CREATE DATABASE IF NOT EXISTS myjdbcdb";
+			stmt.executeUpdate(createDB);
+	        System.out.println("Database created successfully!");
+
+	        
+			stmt.close();
+			con.close();
+		}catch (Exception e) {
+			e.printStackTrace(); 
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
